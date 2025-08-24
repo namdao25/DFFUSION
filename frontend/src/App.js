a75bd969504ed5b30bc46c5bbdf0f12b4f79653d
namdao25 <nam@vndotnet.com>
2025-08-24 09:13:12 -0700

    First Commit, basic infrastructure

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>Welcome to the Model Matching Site</h1>
+      <p>This is a simple React frontend.</p>
+    </div>
+  );
+}
+
+export default App;
